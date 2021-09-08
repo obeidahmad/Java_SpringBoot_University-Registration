8f91a16cd65da56960ec5b27a623c1b60229a91f
Ahmad Obeid <obeid.ahmad2001@outlook.com>
2021-09-08 19:54:02 +0300

    initial commit

---
+++ src/main/java/ai/inmind/universityregistration/UniversityRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ai.inmind.universityregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityRegistrationApplication.class, args);
+    }
+
+}

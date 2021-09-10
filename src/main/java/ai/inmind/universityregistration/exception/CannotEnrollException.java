49fff5264999076481a3ebe6fcdfe6018e414ecf
Ahmad Obeid <obeid.ahmad2001@outlook.com>
2021-09-10 15:17:20 +0300

    fix(Cannot Enroll Exception ErrorMessage): Fix Error Message in CannotEnrollException.java
    Fix Error Message in CannotEnrollException.java, so it can be more fitting in the response message when error is thrown.

--- src/main/java/ai/inmind/universityregistration/exception/CannotEnrollException.java
+++ src/main/java/ai/inmind/universityregistration/exception/CannotEnrollException.java
@@ -3,16 +3,14 @@ package ai.inmind.universityregistration.exception;
 import ai.inmind.universityregistration.model.Class;
 import ai.inmind.universityregistration.model.Student;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
 public class CannotEnrollException extends RuntimeException {
     private static final long serialVersionUID = 2L;
     private final String message;
 
-    public CannotEnrollException(Student student, Class enrollment, String cause) {
-        super(String.format("%s cannot enroll in %s.", student, enrollment), new Throwable(cause));
-        this.message = String.format("%s cannot enroll in %s, because %s", student, enrollment, cause);
+    public CannotEnrollException(Student student, Class aClass, String cause) {
+        super(String.format("Student of id=%s cannot enroll in Class of id=%s.", student.getId(), aClass.getId()), new Throwable(cause));
+        this.message = String.format("Student of id=%s cannot enroll in Class of id=%s, because %s.", student.getId(), aClass.getId(), cause);
     }
 }

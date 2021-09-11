04c9c0d36a69158b4a4a96c849492a24dde0b982
Ahmad Obeid <obeid.ahmad2001@outlook.com>
2021-09-11 15:01:41 +0300

    refactor(Cannot Enroll Exception): Change the Response Message when CannotEnrollException.java is Thrown
    Change the Response Message when CannotEnrollException.java is Thrown for less repetitions

--- src/main/java/ai/inmind/universityregistration/exception/CannotEnrollException.java
+++ src/main/java/ai/inmind/universityregistration/exception/CannotEnrollException.java
@@ -10,10 +10,10 @@ public class CannotEnrollException extends RuntimeException {
     private final String message;
 
     public CannotEnrollException(Long studentId, Long classId, String cause) {
-        super(String.format(LocalizationConfig.messageSource().getMessage("cannotEnroll", null, LocaleParam.getLocale()), studentId, classId),
-                new Throwable(LocalizationConfig.messageSource().getMessage(cause, null, LocaleParam.getLocale())));
+        super(String.format(LocalizationConfig.messageSource().getMessage("cannotEnroll", null, LocaleParam.getLocale()),
+                studentId, classId, LocalizationConfig.messageSource().getMessage(cause, null, LocaleParam.getLocale())));
 
-        this.message = String.format(LocalizationConfig.messageSource().getMessage("cannotEnrollCauseMessage", null, LocaleParam.getLocale()),
+        this.message = String.format(LocalizationConfig.messageSource().getMessage("cannotEnroll", null, LocaleParam.getLocale()),
                 studentId, classId, LocalizationConfig.messageSource().getMessage(cause, null, LocaleParam.getLocale()));
     }
 }
